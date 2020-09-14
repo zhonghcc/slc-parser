78579a1b77f3c0876250aa7d0576a8994be40bc1
zhonghcc <zhonghcc@gmail.com>
2020-09-15 01:11:00 +0800

    read slc file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import SlcModel from './slcmodel';
+import axios from 'axios';
 var model = new SlcModel();
 // const xhr = new XMLHttpRequest();
 // xhr.open("GET", "http://zhonghcc.com/5.slc", true);
@@ -7,5 +8,12 @@ var model = new SlcModel();
 //     console.log(xhr.response)
 // }
 // xhr.send();
+axios.get('http://zhonghcc.com/5.slc')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 model.loadFromFile(1);
 console.log(model);
