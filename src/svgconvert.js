560cbeb4f0c1e119e1cb041562bc4655c5578a42
zhonghcc <zhonghcc@gmail.com>
2020-09-19 01:03:50 +0800

    svg

--- src/svgconvert.js
+++ src/svgconvert.js
@@ -1,5 +1,6 @@
 import SlcModel from './slcmodel'
-import svg from 'svg-builder'
+import { SVG } from '@svgdotjs/svg.js'
+// import {JSDOM} from 'jsdom'
 class SvgConvert{
     constructor(slcModel,foreground,background){
         this.model = slcModel;
@@ -8,23 +9,22 @@ class SvgConvert{
     }
     convert(index){
         let layer = this.model.layers[index];
-        let builder = svg.newInstance();
-        builder.width(800);
-        builder.height(600);
-        builder.rect({width:800,height:600,fill:this.background});
+        // let dom = new JSDOM();
+        let svg = SVG();
+        let rect = svg.rect(800,600);
+        svg.add(rect);
         let boundaries = layer.boundaries;
         for(let i=0;i<boundaries.length;i++){
             let path = "M ";
             let vertexs = boundaries[i].list;
             for(let j=0;j<vertexs.length;j++){
-                path += vertexs[j].x*50+400+" ";
-                path += vertexs[j].y*50+200+" ";
+                path += vertexs[j].x+" ";
+                path += vertexs[j].y+" ";
             }
             path+="z";
-            builder.path({d:path,fill:boundaries[i].outer?this.foreground:this.background});
+            svg.path(path).fill(boundaries[i].outer?this.foreground:this.background);
         }
-        // builder.path({d:'M 100 100 L 300 100 L 200 300 z',stroke:'#FF0000'})
-        let svgOutput = builder.render();
+        let svgOutput = svg.toString();
         console.log(svgOutput);
     }
 }
